4ed8853f040518256fc3b67563a339c933758060
jeeva3771 <jeeva37710@gmail.com>
2024-08-17 16:06:59 +0530

    new one

---
+++ utilityclient.js
@@ -0,0 +1,18 @@
+function mysqlQuery(sql, options, mysqlClient) {
+    return new Promise((resolve, reject) => {
+        try {
+            mysqlClient.query(sql, options || [], (err, rooms) => {
+                if (err) {
+                    return reject(err.sqlMessage)
+                } 
+                resolve(rooms)    
+            })
+        } catch (error) {
+            reject(error.message)
+        }
+    })
+}
+
+module.exports = {
+    mysqlQuery
+}
