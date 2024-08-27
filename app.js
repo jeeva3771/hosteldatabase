604b09f6519e9ff95229133c2a0afd4cbf0c2c1c
jeeva3771 <jeeva37710@gmail.com>
2024-08-27 10:53:27 +0530

    add

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const mysql = require('mysql')
 const logger = require('morgan')
+const path = require('path');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var FileStore = require('session-file-store')(session);
+var fileStoreOptions = {};
 
 //apicontroller
 const course = require('./apicontroller/course.js')
@@ -10,10 +16,27 @@ const blockFloor = require('./apicontroller/blockfloor.js')
 const room = require('./apicontroller/room.js')
 const student = require('./apicontroller/student.js')
 const attendance = require('./apicontroller/attendance.js')
+const homeUi = require('./ui/homeui.js')
 
 const app = express()
 app.use(logger('dev'))
 app.use(express.json())
+app.use(cookieParser());
+app.use(session({
+    store: new FileStore(fileStoreOptions),
+    secret: 'dhoni',
+    resave: true,
+    saveUninitialized: true,
+    cookie : {
+     maxAge:(1000 * 60 * 15)
+   } 
+ }));
+ app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/uicontroller/views'));
 
 app.mysqlClient = mysql.createConnection({
     host: 'localhost',
@@ -35,6 +58,7 @@ app.mysqlClient.connect(function (err) {
         room(app)
         student(app)
         attendance(app)
+        homeUi(app)
 
         app.listen(1000, () => {
             console.log('listen 1000port')
