30a5d3b38af21eae14003b6501213ce01dcd2aad
jeeva3771 <jeeva37710@gmail.com>
2024-09-10 13:09:56 +0530

    m

--- ui/courseui.js
+++ ui/courseui.js
@@ -1,28 +1,7 @@
-// module.exports = (app) => {
-//     app.get('/', async function (req, res) {
-//         const response = await fetch('http://localhost:1000/api/course')
-//         const courses = await response.json()
-//         res.render('pages/course/courselist.ejs', {courses})
-//     })
-// }
-
-const { mysqlQuery } = require("../utilityclient")
-
-async function loadCourse(req, res) {
-    const mysqlClient = req.app.mysqlClient
-    const courses = await mysqlQuery(/*sql*/`SELECT *,
-    DATE_FORMAT(createdAt, "%m-%d-%Y %T") as createdAt,
-    DATE_FORMAT(updatedAt, "%m-%d-%Y %T") as updatedAt
-    FROM course WHERE deletedAt IS NULL`, [],
-    mysqlClient 
-    )
-    res.render('pages/course/courselist.ejs', {courses})
-
+function loadCoursePage(req, res) {
+    res.render('pages/course/courselist.ejs')
 }
-
-
 
 module.exports = (app) => {
-    app.get('/', loadCourse)
+    app.get('/course', loadCoursePage)
 }
-
