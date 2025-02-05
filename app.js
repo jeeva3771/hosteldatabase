8f7841e7a153d54241625c332acf9ba8d3f28a0b
mr sivasankar <sivasankar.k@creativebees.in>
2025-02-05 10:47:32 +0530

    auth modified

--- app.js
+++ app.js
@@ -49,7 +49,11 @@ const dbOptions = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true
+    }
 };
 
 const sessionStore = new MySQLStore(dbOptions);
