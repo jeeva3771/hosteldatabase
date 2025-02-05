dbc56082c7456967552daba946ac66c2dab5397a
mr sivasankar <sivasankar.k@creativebees.in>
2025-02-05 10:11:16 +0530

    connection modified

--- app.js
+++ app.js
@@ -89,7 +89,10 @@ function setupApplication(app) {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME
+        database: process.env.DB_NAME,
+        waitForConnections: true,
+        connectionLimit: 5, // Adjust based on your server limit
+        queueLimit: 0
     })    
 
     app.use(session({
